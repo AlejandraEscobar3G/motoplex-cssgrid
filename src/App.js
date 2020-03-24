f04d3f2e7e9491afa80fa8162b1cc7eb2a505602
Alejandra Escobar <alejandra.escobar@reprisedigital.com>
2020-03-23 21:48:05 -0600

    [Motoplex] - URL Parameters configuration

--- src/App.js
+++ src/App.js
@@ -3,8 +3,9 @@ import React from 'react';
 //Components
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Home from './pages/Home/home';
-import Dealers from './pages/Dealers/dealers';
+import DealersList from './pages/DealersList/dealers';
 import Brands from './pages/Brands/brands';
+import Dealer from './pages/Dealer/dealer';
 import NotFound from './pages/NotFound/not_found';
 
 // Styles
@@ -16,7 +17,8 @@ function App() {
       <div>
         <Switch>
           <Route exact path="/"><Home /></Route>
-          <Route exact path="/distribuidores"><Dealers /></Route>
+          <Route exact path="/distribuidores"><DealersList /></Route>
+          <Route exact path="/distribuidores/:id" children={<Dealer />} />
           <Route exact path="/marcas"><Brands /></Route>
           <Route exact path="*"><NotFound /></Route>
         </Switch>
---
+++ src/pages/Dealer/dealer.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+// Styles
+import './dealer.css';
+
+class Dealer extends React.Component {
+    render() {
+        return(
+        <h1>it woorks</h1>
+        )
+    }
+}
+
+export default Dealer;
